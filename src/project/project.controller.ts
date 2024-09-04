26b5d7c2584292632c2a1a6e47728f07a868e458
Basil Ndonga <basilndonga@gmail.com>
2024-09-04 08:45:43 +0300

    updates on ui

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -34,7 +34,7 @@ export class ProjectController {
   @UseGuards(RolesGuard)
   @Roles(Role.Admin, Role.Manager)
   findAll(@Query('page') page = 1, @Query('perPage') limit = 10) {
-    return this.projectService.findAll(page, limit);
+    return this.projectService.findAll(+page, +limit);
   }
 
   @Get(':id')
